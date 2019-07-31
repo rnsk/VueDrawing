59f72904eac7215779786103f4308c3b3db6deba
rnsk <touch@rnsk.net>
2019-07-31 17:09:19 +0900

    ヘッダーを追加

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import Header from './components/layout/Header';
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
 }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="header">
+        <h1>Canvas</h1>
+        <ul id="nav">
+          <li><router-link to="/">Upload single file</router-link></li>
+          <li><router-link to="/multiple">Upload multiple files</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background-color: #92b0b3;
+    margin-bottom: 20px;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header li {
+    display: inline;
+    list-style: none;
+    padding: 0 10px;
+  }
+
+  .header li + li {
+    border-left: 1px solid #fff;
+  }
+
+  .header li a {
+    color: #fff;
+    text-decoration: none;
+  }
+</style>
